07da6d42a6d67ad712819ae5b5d115889d6c7dda
Dan Rubel <danrubel@google.com>
2016-01-06 17:16:53 -0500

    add settings for device and logging

--- lib/dartino.dart
+++ lib/dartino.dart
@@ -13,12 +13,13 @@ import 'package:logging/logging.dart';
 
 export 'package:atom/atom.dart' show registerPackage;
 
-final Logger _logger = new Logger('atom-dartino');
+const pluginId = 'dartino';
 
 class DartinoDevPackage extends AtomPackage {
-  final Disposables disposables = new Disposables(catchExceptions: true);
+  final Logger _logger = new Logger(pluginId);
+  final Disposables _disposables = new Disposables(catchExceptions: true);
 
-  DartinoDevPackage() : super('dartino');
+  DartinoDevPackage() : super(pluginId);
 
   void activate([dynamic state]) {
     _setupLogging();
@@ -26,30 +27,90 @@ class DartinoDevPackage extends AtomPackage {
     _logger.fine("Running on Chrome version ${process.chromeVersion}.");
 
     // Register commands.
+    _addCmd('atom-workspace', 'dartino:settings',
+        (_) => atom.workspace.open('atom://config/packages/dartino'));
     _addCmd('atom-workspace', 'dartino:rebuild-restart-dev',
         (_) => _rebuildRestart());
     _addCmd('atom-workspace', 'dartino:run-app-on-device',
         (_) => _runAppOnDevice());
   }
 
+  Map config() {
+    return {
+      'devicePath': {
+        'title': 'Device path.',
+        'description': 'The /dev/tty* path for accessing a connected device.',
+        'type': 'string',
+        'default': (isMac ? '/dev/tty.usbmodem14143' : '/dev/ttyACM0'),
+        'order': 1
+      },
+      // development
+      'logging': {
+        'title': 'Log plugin diagnostics to the devtools console.',
+        'description': 'This is for plugin development only!',
+        'type': 'string',
+        'default': 'info',
+        'enum': ['error', 'warning', 'info', 'fine', 'finer'],
+        'order': 3
+      },
+    };
+  }
+
   void deactivate() {
     _logger.info('deactivated');
-    disposables.dispose();
+    _disposables.dispose();
   }
 
   void _addCmd(String target, String command, void callback(AtomEvent e)) {
-    disposables.add(atom.commands.add(target, command, callback));
+    _disposables.add(atom.commands.add(target, command, callback));
   }
+
+  void _setupLogging() {
+    _disposables.add(atom.config.observe('${pluginId}.logging', null, (val) {
+      if (val == null) return;
+      for (Level level in Level.LEVELS) {
+        if (val.toUpperCase() == level.name) {
+          Logger.root.level = level;
+          break;
+        }
+      }
+      _logger.info("logging level: ${Logger.root.level}");
+    }));
+  }
+}
+
+/// Return the path to the dartino plugin, or `null` if unknown.
+String get _dartinoPluginPath {
+  // TODO(danrubel) cleanup this HACK
+  // Find the `dartlang` project.
+  // Directory proj = atom.project.getDirectories().firstWhere(
+  //     (d) => d.getBaseName().endsWith('atom-dartino'), orElse: () => null
+  // );
+  // if (proj == null) {
+  //   atom.notifications.addWarning("Unable to find project '${'dartlang'}'.");
+  //   return new Future.value();
+  // }
+  return '/usr/local/google/home/danrubel/work/git/atom/atom-dartino';
+}
+
+/// Return the path to the Dart SDK, or `null` if unknown.
+String get _sdkPath {
+  // Find the SDK using the dartlang plugin
+  String path = atom.config.getValue('dartlang.sdkLocation');
+  // if (sdk does not exist) {
+  //   showNoSdkMessage();
+  //   return null;
+  // }
+  return path;
 }
 
 /// Compile the Dart sources into web/entry.dart.js then restart Atom
 void _rebuildRestart() {
   atom.notifications.addInfo('Building atom-dartino plugin', dismissable: true);
-  // TODO(danrubel) cleanup this HACK
-  // Find the SDK using the dartlang plugin
-  String sdkPath = '/usr/local/google/home/danrubel/work/eclipse3/dart-sdk';
-  String projPath =
-      '/usr/local/google/home/danrubel/work/git/atom/atom-dartino';
+
+  if (_sdkPath == null || _dartinoPluginPath == null) return;
+  String pubPath = '$_sdkPath/bin/pub';
+  String projPath = _dartinoPluginPath;
 
   // Save any dirty editors.
   atom.workspace.getTextEditors().forEach((editor) {
@@ -57,55 +118,65 @@ void _rebuildRestart() {
   });
 
   // TODO(danrubel) cleanup this HACK
-  String command = '$sdkPath/bin/pub';
   List<String> args = ['run', 'grinder', 'build'];
   ProcessRunner runner;
   // Run process under bash on the mac, to capture the user's env variables.
-  if (process.platform == 'darwin') {
+  if (isMac) {
     //exec('/bin/bash', ['-l', '-c', 'which dart'])
     String arg = args.join(' ');
-    arg = command + ' ' + arg;
+    arg = pubPath + ' ' + arg;
     runner =
         new ProcessRunner('/bin/bash', args: ['-l', '-c', arg], cwd: projPath);
   } else {
-    runner = new ProcessRunner(command, args: args, cwd: projPath);
+    runner = new ProcessRunner(pubPath, args: args, cwd: projPath);
   }
   runner.execSimple().then((result) {
     if (result.exit == 0) {
       atom.notifications.addInfo('Build succeeded', dismissable: true);
       new Future.delayed(new Duration(seconds: 2)).then((_) => atom.reload());
     } else {
-      atom.notifications.addInfo('Build failed',
+      atom.notifications.addError('Build failed',
           dismissable: true, detail: '${result.stdout}\n${result.stderr}');
     }
   });
-}
 
-void _runAppOnDevice() {
-  _logger.log(Level.INFO, 'runAppOnDevice');
-  Notification _notification;
-  _notification = atom.notifications
-      .addInfo('Run app on device?', dismissable: true, buttons: [
-    new NotificationButton('Run…', () {
-      _notification.dismiss();
-      _logger.log(Level.INFO, 'runAppOnDevice - run clicked');
-    })
-  ]);
-}
-
-void _setupLogging() {
-  // TODO(danrubel) cleanup this HACK
-  Logger.root.level = Level.FINE;
-  // disposables.add(atom.config.observe('${pluginId}.logging', null, (val) {
-  //   if (val == null) return;
-  //
-  //   for (Level level in Level.LEVELS) {
-  //     if (val.toUpperCase() == level.name) {
-  //       Logger.root.level = level;
-  //       break;
+  // return new PubRunJob.local(projPath, args, title: name).schedule().then(
+  //     (JobStatus status) {
+  //   // Check for an exit code of `0` from grind build.
+  //   if (status.isOk && status.result == 0) {
+  //     if (runTests) {
+  //       _runTests();
+  //     } else {
+  //       new Future.delayed(new Duration(seconds: 2)).then((_) => atom.reload());
   //     }
   //   }
-  //
-  //   _logger.info("logging level: ${Logger.root.level}");
-  // }));
+  // });
+}
+
+_runAppOnDevice() async {
+  if (_sdkPath == null || _dartinoPluginPath == null) return;
+  String dartPath = '$_sdkPath/bin/dart';
+  String projPath = _dartinoPluginPath;
+  String appPath = '$projPath/bin/device_tty.dart';
+  String ttyPath = atom.config.getValue('$pluginId.devicePath');
+  if (ttyPath == null || ttyPath.trim().isEmpty) {
+    atom.notifications.addError('Device path is not set.',
+        detail: 'Please set the device path in '
+            'Settings > Packages > $pluginId > Device Path',
+        dismissable: true);
+    return;
+  }
+
+  ProcessRunner runner = new ProcessRunner(dartPath,
+      args: [appPath, ttyPath, 'fletch run'], cwd: projPath);
+  runner.execSimple().then((result) {
+    if (result.exit == 0) {
+      atom.notifications.addInfo('Launched app on device', dismissable: true);
+    } else {
+      atom.notifications.addError('Launch failed',
+          dismissable: true,
+          detail: 'exit code ${result.exit}\n${result.stdout}'
+              '\n${result.stderr}');
+    }
+  });
 }

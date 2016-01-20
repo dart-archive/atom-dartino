d16dac3ae65cb4f99f45d823938461470a079265
danrubel <danrubel@google.com>
2016-01-20 15:34:54 -0800

    cleanup runOnDevice and remove unneeded rebuild cmd

--- lib/dartino.dart
+++ lib/dartino.dart
@@ -4,9 +4,8 @@
 
 library atom.dartino.plugin;
 
-import 'dart:async';
-
 import 'package:atom/atom.dart';
+import 'package:atom/node/fs.dart';
 import 'package:atom/node/process.dart';
 import 'package:atom/utils/disposable.dart';
 import 'package:logging/logging.dart';
@@ -29,8 +28,6 @@ class DartinoDevPackage extends AtomPackage {
     // Register commands.
     _addCmd('atom-workspace', 'dartino:settings',
         (_) => atom.workspace.open('atom://config/packages/dartino'));
-    _addCmd('atom-workspace', 'dartino:rebuild-restart-dev',
-        (_) => _rebuildRestart());
     _addCmd('atom-workspace', 'dartino:run-app-on-device',
         (_) => _runAppOnDevice());
   }
@@ -79,7 +76,8 @@ class DartinoDevPackage extends AtomPackage {
   }
 }
 
-/// Return the path to the dartino plugin, or `null` if unknown.
+/// Return the path to the dartino plugin.
+/// If not found, report an error to the user and return `null`.
 String get _dartinoPluginPath {
   // TODO(danrubel) cleanup this HACK
   // Find the `dartlang` project.
@@ -90,74 +88,62 @@ String get _dartinoPluginPath {
   //   atom.notifications.addWarning("Unable to find project '${'dartlang'}'.");
   //   return new Future.value();
   // }
+  if (isMac) return '/Users/danrubel/work/git/atom/atom-dartino';
   return '/usr/local/google/home/danrubel/work/git/atom/atom-dartino';
 }
 
-/// Return the path to the Dart SDK, or `null` if unknown.
-String get _sdkPath {
-  // Find the SDK using the dartlang plugin
-  String path = atom.config.getValue('dartlang.sdkLocation');
-  // if (sdk does not exist) {
-  //   showNoSdkMessage();
-  //   return null;
-  // }
+String get _deviceTtyPath {
+  if (_dartinoPluginPath == null) return null;
+  String path = fs.join(_dartinoPluginPath, 'bin', 'device_tty.dart');
+  if (!fs.existsSync(path)) {
+    atom.notifications.addError('Cannot find Device TTY utility.',
+        detail: 'Cannot find Device TTY utility at $path', dismissable: true);
+    return null;
+  }
   return path;
 }
 
-/// Compile the Dart sources into web/entry.dart.js then restart Atom
-void _rebuildRestart() {
-  atom.notifications.addInfo('Building atom-dartino plugin', dismissable: true);
-
-  if (_sdkPath == null || _dartinoPluginPath == null) return;
-  String pubPath = '$_sdkPath/bin/pub';
-  String projPath = _dartinoPluginPath;
-
-  // Save any dirty editors.
-  atom.workspace.getTextEditors().forEach((editor) {
-    if (editor.isModified()) editor.save();
-  });
-
-  // TODO(danrubel) cleanup this HACK
-  List<String> args = ['run', 'grinder', 'build'];
-  ProcessRunner runner;
-  // Run process under bash on the mac, to capture the user's env variables.
-  if (isMac) {
-    //exec('/bin/bash', ['-l', '-c', 'which dart'])
-    String arg = args.join(' ');
-    arg = pubPath + ' ' + arg;
-    runner =
-        new ProcessRunner('/bin/bash', args: ['-l', '-c', arg], cwd: projPath);
-  } else {
-    runner = new ProcessRunner(pubPath, args: args, cwd: projPath);
+/// Return the path to the Dart SDK as configured in the dartlang plugin.
+/// If not found, report an error to the user and return `null`.
+String get _sdkPath {
+  String path = atom.config.getValue('dartlang.sdkLocation');
+  if (path == null || path.trim().isEmpty) {
+    atom.notifications.addError('Dart SDK is not set.',
+        detail: 'Please set the Dart SDK path in '
+            'Settings > Packages > dartlang > Dart SDK Location',
+        dismissable: true);
+    return null;
   }
-  runner.execSimple().then((result) {
-    if (result.exit == 0) {
-      atom.notifications.addInfo('Build succeeded', dismissable: true);
-      new Future.delayed(new Duration(seconds: 2)).then((_) => atom.reload());
-    } else {
-      atom.notifications.addError('Build failed',
-          dismissable: true, detail: '${result.stdout}\n${result.stderr}');
-    }
-  });
+  if (!fs.existsSync(path)) {
+    atom.notifications.addError('Cannot find Dart SDK.',
+        detail: 'Cannot find Dart SDK at $path. '
+            'Please set the Dart SDK path in '
+            'Settings > Packages > dartlang > Dart SDK Location',
+        dismissable: true);
+    return null;
+  }
+  return path;
+}
 
-  // return new PubRunJob.local(projPath, args, title: name).schedule().then(
-  //     (JobStatus status) {
-  //   // Check for an exit code of `0` from grind build.
-  //   if (status.isOk && status.result == 0) {
-  //     if (runTests) {
-  //       _runTests();
-  //     } else {
-  //       new Future.delayed(new Duration(seconds: 2)).then((_) => atom.reload());
-  //     }
-  //   }
-  // });
+/// Return the path to the VM in the Dart SDK.
+/// If not found, report an error to the user and return `null`.
+String get _vmPath {
+  if (_sdkPath == null) return null;
+  String path = fs.join(_sdkPath, 'bin', 'dart');
+  if (!fs.existsSync(path)) {
+    atom.notifications.addError('Cannot find Dart VM in the Dart SDK.',
+        detail: 'Cannot find Dart VM at $path. '
+            'Please set the Dart SDK path in '
+            'Settings > Packages > dartlang > Dart SDK Location',
+        dismissable: true);
+    return null;
+  }
+  return path;
 }
 
 _runAppOnDevice() async {
-  if (_sdkPath == null || _dartinoPluginPath == null) return;
-  String dartPath = '$_sdkPath/bin/dart';
-  String projPath = _dartinoPluginPath;
-  String appPath = '$projPath/bin/device_tty.dart';
+  if (_vmPath == null || _deviceTtyPath == null)
+    return;
   String ttyPath = atom.config.getValue('$pluginId.devicePath');
   if (ttyPath == null || ttyPath.trim().isEmpty) {
     atom.notifications.addError('Device path is not set.',
@@ -166,9 +152,16 @@ _runAppOnDevice() async {
         dismissable: true);
     return;
   }
-
-  ProcessRunner runner = new ProcessRunner(dartPath,
-      args: [appPath, ttyPath, 'fletch run'], cwd: projPath);
+  if (!fs.existsSync(ttyPath)) {
+    atom.notifications.addError('Cannot find device.',
+        detail: 'Cannot find device at $ttyPath. '
+         'Please connect the device or set the device path in '
+            'Settings > Packages > $pluginId > Device Path',
+        dismissable: true);
+    return;
+  }
+  ProcessRunner runner = new ProcessRunner(_vmPath,
+      args: [_deviceTtyPath, ttyPath, 'fletch run'], cwd: _dartinoPluginPath);
   runner.execSimple().then((result) {
     if (result.exit == 0) {
       atom.notifications.addInfo('Launched app on device', dismissable: true);
